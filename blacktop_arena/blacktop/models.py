eef06826539fe3042c046aee3e9f15aa0cbfc7da
vladkim91 <vkcoding21@gmail.com>
2022-02-07 15:49:23 -0800

    models added

--- blacktop_arena/blacktop/models.py
+++ blacktop_arena/blacktop/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    wins = models.IntegerField(default=0)
+    losses = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.username
+
+
+class Game(models.Model):
+    stats = models.JSONField("stats")
+    winner = models.ForeignKey(
+        User, related_name='winner', null=True, default=None, on_delete=models.CASCADE, blank=True
+    )
+    date = models.DateField(
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=100)
+    height = models.IntegerField()
+    attributes = models.JSONField("attributes")
+    tendencies = models.JSONField("tendencies")
+    avatar = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
+    current_team = models.ForeignKey(
+        User, related_name='current_team', null=True, default=None, on_delete=models.PROTECT
+    )
+    user_team = models.ForeignKey(
+        Game, related_name='user_team', null=True, default=None, on_delete=models.PROTECT
+    )
+    cpu_team = models.ForeignKey(
+        Game, related_name='cpu_team', null=True, default=None, on_delete=models.PROTECT
+    )
+
+    def __str__(self):
+        return self.name
+
--- blacktop_arena/blacktop_django/settings.py
+++ blacktop_arena/blacktop_django/settings.py
@@ -73,14 +73,15 @@ WSGI_APPLICATION = 'blacktop_django.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blacktop',
+        'USER': 'blacktopuser',
+        'PASSWORD': 'blacktop',
+        'HOST': 'localhost'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
